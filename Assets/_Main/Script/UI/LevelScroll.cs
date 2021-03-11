4bafa26b4b3c06852d4f6f80f4895aad406ff9af
anviettrung <an.viettrung.m@gmail.com>
2021-03-11 16:12:44 +0700

    tmp | scroll view

---
+++ Assets/_Main/Script/UI/LevelScroll.cs
@@ -0,0 +1,39 @@
+﻿using Mopsicus.InfiniteScroll;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelScroll : MonoBehaviour
+{
+
+	[SerializeField]
+	private InfiniteScroll Scroll;
+
+	[SerializeField]
+	private int Count = 100;
+	public int height = 400;
+
+	void Start()
+	{
+		Scroll.OnFill += OnFillItem;
+		Scroll.OnHeight += OnHeightItem;
+
+		Scroll.InitData(Count);
+	}
+
+	void OnFillItem(int index, GameObject item)
+	{
+//		item.GetComponentInChildren<Text>().text = index.ToString();
+	}
+
+	int OnHeightItem(int index)
+	{
+		return height;
+	}
+
+	public void SceneLoad(int index)
+	{
+		SceneManager.LoadScene(index);
+	}
+
+}
