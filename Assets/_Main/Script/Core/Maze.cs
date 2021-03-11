e370792e56c90fd5a034928ab750ff248d056b58
anviettrung <an.viettrung.m@gmail.com>
2021-03-11 14:12:51 +0700

    spawn bug

--- Assets/_Main/Script/Core/Maze.cs
+++ Assets/_Main/Script/Core/Maze.cs
@@ -6,6 +6,8 @@ public class Maze : MonoBehaviour
 {
 	public MazeData mazeData;
 	public CellsData cellsData;
+	public GameObject bugPrefab;
+	protected Vector3 offset;
 
 	private void Start()
 	{
@@ -14,17 +16,25 @@ public class Maze : MonoBehaviour
 
 	public void SpawnMaze()
 	{
-		Vector3 offset = new Vector3(-mazeData.width / 2.0f + 0.5f, mazeData.height / 2.0f - 0.5f, 0);
+		offset = new Vector3(-mazeData.width / 2.0f + 0.5f, mazeData.height / 2.0f - 0.5f, 0);
 
 		for (int y = 0; y < mazeData.height; y++) {
 			for (int x = 0; x < mazeData.width; x++) {
-				SpriteRenderer cell = Instantiate(cellsData.Prefab);
+				SpriteRenderer cell = SpawnEntity(cellsData.Prefab.gameObject, x, y).GetComponent<SpriteRenderer>();
 				cell.name = "Cell[" + x +  "," + y + "]";
-				cell.transform.SetParent(this.transform);
-				cell.transform.localPosition = new Vector3(x, -y, 0) + offset;
-
 				cell.sprite = cellsData.Arts[mazeData.GetCell(x, y)];
 			}
 		}
+
+		SpawnEntity(bugPrefab, 0, 0);
+	}
+
+	public GameObject SpawnEntity(GameObject prefab, int x, int y)
+	{
+		var clone = Instantiate(prefab);
+		clone.transform.SetParent(this.transform);
+		clone.transform.position = new Vector3(x, -y, 0) + offset;
+
+		return clone;
 	}
 }
